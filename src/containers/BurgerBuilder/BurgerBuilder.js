15e930eb54adc769e5bae2b0f8396f244934abc1
Michael Neely <michael@michaelneely.net>
2019-10-11 23:49:05 -0400

    Added the price to order modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,10 @@ purchaseCancelHandler = () => {
     this.setState({purchasing: false});
 }
 
+purchaseContinueHandler = () => {
+    alert('You continued!');
+}
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -88,7 +92,11 @@ purchaseCancelHandler = () => {
         return(
             <Auxiliary>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        price={this.state.totalPrice}
+                        purchaseCancelled={this.purchaseCancelHandler} 
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
