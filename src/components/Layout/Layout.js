3fa4376c661d09df02cb3d2e8178ad29ca130751
Michael Neely <michael@michaelneely.net>
2019-10-22 01:09:07 -0400

    Added mobile responsive logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,15 @@ import classes from './Layout.module.css';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxiliary>
-    <Toolbar />
-    <main className={classes.Content}>
-        {props.children}
-    </main>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Auxiliary>
 );
 
