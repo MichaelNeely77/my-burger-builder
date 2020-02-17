60be69a8e65a3e2a0ce17fb6e614142c07a12ce5
Michael Neely <michael@michaelneely.net>
2020-02-16 22:30:11 -0500

    Got started on the Redux for Burger Builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,11 +18,11 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
     state = {
-        ingredients: null,
-        totalPrice: 4,
+
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount() {
